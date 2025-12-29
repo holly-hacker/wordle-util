f777acb25a456d33796758d1fb5795064372a5d8
Variant9 <dario.db@hotmail.com>
2025-12-29 15:42:46 +0100

    Adjust introduction text

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -70,10 +70,11 @@ pub fn Introduction() -> impl IntoView {
         <div class="introduction">
             <h1>"Wordle Checker"</h1>
             <p>"This is a utility to give insight into your wordle play."</p>
-            <p>
-                "Enter your guesses below and click the letters to change their type. "
-                "You can right-click a letter to remove it."
-            </p>
+            <ul>
+                <li>"Type to enter letters"</li>
+                <li>"Left-click an entered letter to change its type"</li>
+                <li>"Use backspace or right-click to remove letters"</li>
+            </ul>
         </div>
     }
 }
